170ff7a90115e3d2cbb8f146c9fb3e7d949a2e44
drakepotter <drakepotter03@gmail.com>
2026-02-06 20:11:18 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,56 +1,91 @@
-//[Your Name Here]
+//[Drake Potter]
 //COSC 3750
-//[Date Here]
-//
-/*
-	Don't forget to run your go mod init command in your terminal
-	Review the assignment instructions for running your code
-	All the code you need to write should be put in the /ds/ package files
-	Uncomment the import statement for your module name
-	you can uncomment the tests in main as you go to test
-	The code in main is not an extensive test, you should add more and test your code as needed
-*/
+//[02/06/26]
+
 package main
 
 import (
 	"fmt"
-	/*"hw01/ds" */ //this needs to be changes to your module name and uncommented
+	"hw-linked/ds"
 )
 
 func main() {
 	fmt.Println("Only here so the import doesn't leave an error")
 
-	//linkedlist := &ds.LinkedList{}
-	//linkedlist.InsertAt(0, "first")
-	//linkedlist.Insert("first")
-	//linkedlist.Insert("first")
-	//linkedlist.Insert("second")
-	//linkedlist.Insert("third")
-	//linkedlist.Insert("fourth")
-	//linkedlist.Insert("fifth")
-	//linkedlist.RemoveAt(4)
-	//linkedlist.PrintList()
-	//fmt.Println("The size of the linked list is:", linkedlist.GetSize())
-	//fmt.Println("-------------")
-	//linkedlist.RemoveAll("first")
-	//linkedlist.PrintList()
-	//fmt.Println("-------------")
-	//linkedlist.Reverse()
-	//linkedlist.PrintList()
-	//fmt.Println("The size of the linked list is:", linkedlist.GetSize())
-	//fmt.Println("-------------")
+	linkedlist := &ds.LinkedList{}
+	linkedlist.InsertAt(0, "first")
+	linkedlist.Insert("first")
+	linkedlist.Insert("first")
+	linkedlist.Insert("second")
+	linkedlist.Insert("third")
+	linkedlist.Insert("fourth")
+	linkedlist.Insert("fifth")
+	linkedlist.RemoveAt(4)
+	linkedlist.PrintList()
+	fmt.Println("The size of the linked list is:", linkedlist.GetSize())
+	fmt.Println("-------------")
+	linkedlist.RemoveAll("first")
+	linkedlist.PrintList()
+	fmt.Println("-------------")
+	linkedlist.Reverse()
+	linkedlist.PrintList()
+	fmt.Println("The size of the linked list is:", linkedlist.GetSize())
+	fmt.Println("-------------")
 
-	//stack := &ds.Stack{}
-	//stack.Push("first")
-	//stack.Push("second")
-	//stack.Push("third")
-	//data, _ := stack.Pop()
-	//println("Popped from stack:", data)
+	stack := &ds.Stack{}
+	stack.Push("first")
+	stack.Push("second")
+	stack.Push("third")
+	data, _ := stack.Pop()
+	println("Popped from stack:", data)
 
-	//queue := &ds.Queue{}
-	//queue.Push("first")
-	//queue.Push("second")
-	//queue.Push("third")
-	//data, _ = queue.Pop()
-	//println("Popped from queue:", data)
+	queue := &ds.Queue{}
+	queue.Push("first")
+	queue.Push("second")
+	queue.Push("third")
+	data, _ = queue.Pop()
+	println("Popped from queue:", data)
+
+	//LinkedList tests
+	l := ds.LinkedList{}
+	l.Insert("Donkey")
+	l.PrintList()
+
+	l.InsertAt(0, "Shrek")
+	l.PrintList()
+
+	l.Remove("Donkey")
+	l.PrintList()
+
+	if l.GetSize() == 1 {
+		fmt.Println("Size is correct")
+	}
+
+	for i := 0; i < 4; i++ {
+		l.Insert("Farquaad")
+	}
+	l.PrintList()
+
+	l.Reverse()
+	l.PrintList()
+
+	l.RemoveAt(4)
+	l.PrintList()
+
+	l.RemoveAll("Farquaad")
+	if l.IsEmpty() {
+		fmt.Println("Empty")
+	}
+
+	//Stack tests
+	var stack2 ds.Stack
+	stack2.Push("Bob")
+	stack2.Push("Pat")
+	fmt.Println(stack2.Pop())
+
+	//Queue tests
+	var queue2 ds.Queue
+	queue2.Push("Lester")
+	queue2.Push("Woody")
+	fmt.Println(queue2.Pop())
 }
